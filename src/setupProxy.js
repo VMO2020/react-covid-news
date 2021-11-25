226c9af6ecc6cabe5f2bfba5578d8701d4796500
VMO2020 <victor.ottati@gmail.com>
2021-11-25 16:37:02 +0000

    fix proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
 	app.use(
 		'/news',
 		createProxyMiddleware({
-			target: 'https://covid-19-wordnews-api.herokuapp.com',
+			target: '*',
 			changeOrigin: true,
 		})
 	);
